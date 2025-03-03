7d244dd33fc2571de0ca27eaa85d66acb390e124
Bobeica Nikita <niki.bobeica@gmail.com>
2025-03-03 16:44:01 +0100

    restart setup foundation

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.farmmyfarm {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.farmmyfarm to javafx.fxml;
-    exports com.example.farmmyfarm;
-}
